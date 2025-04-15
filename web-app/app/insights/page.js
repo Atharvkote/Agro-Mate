720fe9ca4a81123460b6372ab08bfe7279487d86
Atharva Kote <atharvkote3@gmail.com>
2025-04-15 23:38:01 +0530

    commit

--- web-app/app/insights/page.js
+++ web-app/app/insights/page.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Info } from 'lucide-react'
 
 const page = () => {
   return (
-    <div>
-      
+    <div className='flex h-screen w-screen justify-center items-center'>
+      <div className='bg-green-100 rounded-2xl flex justify-center items-center flex-col gap-3 lg:w-1/2 lg:h-1/2 p-5'>
+        <Info className='w-28 h-28 text-green-600' />
+        <p className='lg:text-3xl font-bold text-green-600'>No Insights Available Right Now</p>
+      </div>
     </div>
   )
 }
